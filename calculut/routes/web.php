82fa85e86237c6ec9d5e0dca01c77e3cdc764c6e
Léo Péron <peronleo8@gmail.com>
2024-05-09 09:27:41 +0200

    blabla

--- calculut/routes/web.php
+++ calculut/routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auth',[\App\Http\Controllers\Connexion::class,'auth'])->name('auth_route');
+Route::get('/logout',[\App\Http\Controllers\Connexion::class,'logout'])->name('logout_route');
+
 Route::get('/{vue_capture?}', function () {
     return view('welcome');
 })->where('vue_capture', '[\/\w\.-]*');
